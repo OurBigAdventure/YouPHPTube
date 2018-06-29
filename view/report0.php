8a6c3879568ea7f874879e0d42f089b6d11df1b1
Vinzenz Hersche <hersche@openmailbox.org>
2018-06-30 01:15:49 +0200

    fix first dashboard-site

--- view/report0.php
+++ view/report0.php
@@ -1,8 +1,8 @@
 
 <div class="row dashboard">
     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-        <div class="panel panel-blue">
-            <div class="panel-heading">
+        <div class="card bg-secondary">
+            <div class="card-heading">
                 <div class="row">
                     <div class="col-xs-3">
                         <i class="fa fa-play-circle fa-5x"></i>
@@ -14,17 +14,17 @@
                 </div>
             </div>
             <a href="<?php echo $global['webSiteRootURL']; ?>mvideos">
-                <div class="panel-footer">
-                    <span class="float-left"><?php echo __("View Details"); ?></span>
-                    <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
+                <div class="card-footer">
+                    <span class="text-primary float-left"><?php echo __("View Details"); ?></span>
+                    <span class="text-primary float-right"><i class="fa fa-arrow-circle-right"></i></span>
                     <div class="clearfix"></div>
                 </div>
             </a>
         </div>
     </div>
     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-        <div class="panel panel-primary">
-            <div class="panel-heading">
+        <div class="card bg-primary">
+            <div class="card-heading">
                 <div class="row">
                     <div class="col-xs-3">
                         <i class="fa fa-eye fa-5x"></i>
@@ -36,7 +36,7 @@
                 </div>
             </div>
             <a href="<?php echo $global['webSiteRootURL']; ?>mvideos">
-                <div class="panel-footer">
+                <div class="card-footer">
                     <span class="float-left"><?php echo __("View Details"); ?></span>
                     <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
                     <div class="clearfix"></div>
@@ -45,8 +45,8 @@
         </div>
     </div>
     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-        <div class="panel panel-purple">
-            <div class="panel-heading">
+        <div class="card bg-info">
+            <div class="card-heading">
                 <div class="row">
                     <div class="col-xs-3">
                         <i class="fa fa-users fa-5x"></i>
@@ -58,7 +58,7 @@
                 </div>
             </div>
             <a href="<?php echo $global['webSiteRootURL']; ?>users">
-                <div class="panel-footer">
+                <div class="card-footer">
                     <span class="float-left"><?php echo __("View Details"); ?></span>
                     <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
                     <div class="clearfix"></div>
@@ -67,8 +67,8 @@
         </div>
     </div>
     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-        <div class="panel panel-wine">
-            <div class="panel-heading">
+        <div class="card bg-warning">
+            <div class="card-heading">
                 <div class="row">
                     <div class="col-xs-3">
                         <i class="fa fa-user-plus fa-5x"></i>
@@ -80,7 +80,7 @@
                 </div>
             </div>
             <a href="<?php echo $global['webSiteRootURL']; ?>subscribes">
-                <div class="panel-footer">
+                <div class="card-footer">
                     <span class="float-left"><?php echo __("View Details"); ?></span>
                     <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
                     <div class="clearfix"></div>
@@ -91,8 +91,8 @@
 
 
     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-        <div class="panel panel-red">
-            <div class="panel-heading">
+        <div class="card bg-warning">
+            <div class="card-heading">
                 <div class="row">
                     <div class="col-xs-3">
                         <i class="fa fa-comments fa-5x"></i>
@@ -104,7 +104,7 @@
                 </div>
             </div>
             <a href="<?php echo $global['webSiteRootURL']; ?>comments">
-                <div class="panel-footer">
+                <div class="card-footer">
                     <span class="float-left"><?php echo __("View Details"); ?></span>
                     <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
                     <div class="clearfix"></div>
@@ -113,11 +113,11 @@
         </div>
     </div>
     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-        <div class="panel panel-orange">
-            <div class="panel-heading">
+        <div class="card bg-danger">
+            <div class="card-heading">
                 <div class="row">
                     <div class="col-xs-3">
-                        <i class="fa fa-thumbs-o-up fa-5x"></i>
+                        <i class="fas fa-thumbs-up fa-5x"></i>
                     </div>
                     <div class="col-xs-9 text-right">
                         <div class="huge loading" id="totalVideosLikes">0</div>
@@ -126,20 +126,20 @@
                 </div>
             </div>
             <a href="<?php echo $global['webSiteRootURL']; ?>mvideos">
-                <div class="panel-footer">
-                    <span class="float-left"><?php echo __("View Details"); ?></span>
-                    <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
+                <div class="card-footer">
+                    <span class="text-primary float-left"><?php echo __("View Details"); ?></span>
+                    <span class="text-primary float-right"><i class="fa fa-arrow-circle-right"></i></span>
                     <div class="clearfix"></div>
                 </div>
             </a>
         </div>
     </div>
     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-        <div class="panel panel-yellow">
-            <div class="panel-heading">
+        <div class="card bg-light">
+            <div class="card-heading">
                 <div class="row">
                     <div class="col-xs-3">
-                        <i class="fa fa-thumbs-o-down fa-5x"></i>
+                        <i class="fas fa-thumbs-down fa-5x"></i>
                     </div>
                     <div class="col-xs-9 text-right">
                         <div class="huge loading" id="totalVideosDislikes">0</div>
@@ -148,7 +148,7 @@
                 </div>
             </div>
             <a href="<?php echo $global['webSiteRootURL']; ?>mvideos">
-                <div class="panel-footer">
+                <div class="card-footer">
                     <span class="float-left"><?php echo __("View Details"); ?></span>
                     <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
                     <div class="clearfix"></div>
@@ -157,11 +157,11 @@
         </div>
     </div>
     <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
-        <div class="panel panel-green">
-            <div class="panel-heading">
+        <div class="card bg-success">
+            <div class="card-heading">
                 <div class="row">
                     <div class="col-xs-3">
-                        <i class="fa fa-clock-o fa-5x"></i>
+                        <i class="fas fa-clock fa-5x"></i>
                     </div>
                     <div class="col-xs-9 text-right">
                         <div class="huge loading" id="totalDurationVideos">0</div>
@@ -170,9 +170,9 @@
                 </div>
             </div>
             <a href="<?php echo $global['webSiteRootURL']; ?>mvideos">
-                <div class="panel-footer">
+                <div class="card-footer">
                     <span class="float-left"><?php echo __("View Details"); ?></span>
-                    <span class="float-right"><i class="fa fa-arrow-circle-right"></i></span>
+                    <span class="float-right"><i class="fas fa-arrow-circle-right"></i></span>
                     <div class="clearfix"></div>
                 </div>
             </a>
@@ -191,6 +191,7 @@
             --></div>
     </div>
 </nav>
+<div class="row">
 <div class="col-md-3">
 
     <div class="panel panel-default">
@@ -250,6 +251,7 @@
             </div>
         </div>
     </div>
+</div>
 </div>
 <script>
 
