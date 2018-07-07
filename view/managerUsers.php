9e951dabd13e289a6560caee8e9ba73fc79e1675
Vinzenz Hersche <hersche@openmailbox.org>
2018-07-07 04:49:15 +0200

    fix modal's close

--- view/managerUsers.php
+++ view/managerUsers.php
@@ -58,8 +58,10 @@ $userGroups = UserGroups::getAllUsersGroups();
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                            <h4 class="modal-title"><?php echo __("User Form"); ?></h4>
+                          <h4 class="modal-title"><?php echo __("User Form"); ?></h4>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
                         </div>
                         <div class="modal-body">
                             <form class="form-compact"  id="updateUserForm" onsubmit="">
--- view/videoComments.php
+++ view/videoComments.php
@@ -106,8 +106,10 @@ if (User::canSeeCommentTextarea()) {
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="modal-header">
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                     <h4 class="modal-title"><?php echo __("Comment Form"); ?></h4>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
                 </div>
                 <div class="modal-body">
                     <input type="hidden" value="" id="inputEditCommentId"/>
